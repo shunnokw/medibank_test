278b7dcd086d7ba299c187a64630d8aaf35f55bf
Jason Wong <nok10211021@gmail.com>
2023-01-18 17:16:46 +1100

    ✨ Init commit

---
+++ newsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  newsApp
+//
+//  Created by Jason Wong on 18/1/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .blue
+    }
+
+
+}
+
